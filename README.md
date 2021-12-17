f16a31b6686783132e21ea2b70f049e6afe1c709
Martin Castro <martin.castro>
2021-12-17 11:06:49 +0100

    Quick fix
    modified: README.md
    modified: .gitignore
    modified: jackkruse.py

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 Web crawlers using Python3 & Beautiful Soup.
 
 ![alt-img](./wallpaper.jpeg)
+
+## Instructions
+```bash
+virtualenv -p python3 .env
+source .env/bin/activate
+pip install -r requirements.txt
+```
+Now adapt and run each script independently:
+```bash
+python3 jackkruse.py
+```
--- jackkruse.py
+++ jackkruse.py
@@ -14,21 +14,24 @@ ROOT: str = "https://jackkruse.com/blogindex/page/{page}/"
 
 
 def save(filename: str, data: str) -> None:
-    path: str = os.path.join(os.sep, 'tmp', filename)
-    with open(path, 'w') as file_handler:
+    """Saves `data` into a cache file in the /tmp called `filename`."""
+    path: str = os.path.join(os.sep, "tmp", filename)
+    with open(path, "w") as file_handler:
         file_handler.write(data)
 
 
 def get(filename: str) -> str:
-    path: str = os.path.join(os.sep, 'tmp', filename)
+    """Loads data from a cache file in the /tmp called `filename`, if it exists."""
+    path: str = os.path.join(os.sep, "tmp", filename)
     if not os.path.isfile(path):
         return ""
-    with open(path, 'r') as file_handler:
+    with open(path, "r") as file_handler:
         return file_handler.read()
 
 
 def load(url: str) -> str:
-    filename: str = hashlib.md5(url.encode('utf-8')).hexdigest() + ".html"
+    """Downloads an HTML from the `url` if it doesn't exist in the cache."""
+    filename: str = hashlib.md5(url.encode("utf-8")).hexdigest() + ".html"
     cached: str = get(filename)
     if cached:
         return cached
@@ -41,15 +44,18 @@ def load(url: str) -> str:
 
 
 def pdf(url: str) -> str:
+    """Exports `url` as a PDF if it doesn't exist in the cache."""
     print(f"CRAWL: {url}")
     if not url:
         return
     url: str = url.strip() + "?print=print"
-    filename: str = hashlib.md5(url.encode('utf-8')).hexdigest() + ".pdf"
+    filename: str = (
+        hashlib.md5(url.encode("utf-8")).hexdigest() + ".pdf"
+    )
     assert url
     assert filename
-    if not os.path.isdir('pdf'):
-        os.mkdir('pdf')
+    if not os.path.isdir("pdf"):
+        os.mkdir("pdf")
     path: str = os.path.join("pdf", filename)
     if not os.path.isfile(path):
         for i in range(ATTEMPTS):
@@ -63,11 +69,12 @@ def pdf(url: str) -> str:
                 continue
 
 
-def links(html: str) -> typying.Generator[str, None, None]:
+def links(html: str) -> typing.Generator[str, None, None]:
+    """Returns a list of links found in the `html` string."""
     soup = BeautifulSoup(html, "html.parser")
-    for link in soup.find_all('a', text=re.compile("Read More")):
-        url = link.get('href')
-        if url and url.startswith('http'):
+    for link in soup.find_all("a", text=re.compile("Read More")):
+        url = link.get("href")
+        if url and url.startswith("http"):
             yield url
 
 
